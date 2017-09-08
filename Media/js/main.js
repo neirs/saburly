fbc45482ead97db814a4015c09bfae2297738b89
neir <31209278+neirs@users.noreply.github.com>
2017-09-08 18:09:14 +0200

    section galerija

---
+++ Media/js/main.js
@@ -0,0 +1,17 @@
+var pozSlike=0;
+var slike=["Analytical Capabilities.svg","attendance roster.svg","kivi.svg","ribica.svg","vsm.svg","zoblak.svg"];
+
+function switchImage(promjena)
+{
+    var location="media\\our work\\";
+    if(promjena=="+" && pozSlike<slike.length-1) {
+        document.getElementById("galerijaSlika").src=location+slike[++pozSlike];
+        console.log("slika je"+location+slike[pozSlike]);
+        
+    }
+    if(promjena=="-" && pozSlike>0){
+        document.getElementById("galerijaSlika").src=location+slike[--pozSlike]; 
+        console.log("slika je"+location+slike[pozSlike]);        
+    }
+};
+
